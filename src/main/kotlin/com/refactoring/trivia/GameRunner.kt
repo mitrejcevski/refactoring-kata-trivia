b1c75cce0b14da88ae842509659961bf319ecae7
Jov Mit <mitrejcevski@gmail.com>
2021-12-11 22:53:19 +0100

    Initial kata setup

---
+++ src/main/kotlin/com/refactoring/trivia/GameRunner.kt
@@ -0,0 +1,31 @@
+package com.refactoring.trivia
+
+import java.util.*
+
+object GameRunner {
+    var notAWinner: Boolean = false
+}
+
+fun main(args: Array<String>) {
+    val aGame = Game()
+
+    aGame.add("Chet")
+    aGame.add("Pat")
+    aGame.add("Sue")
+
+    val rand = Random()
+
+    do {
+
+        aGame.roll(rand.nextInt(5) + 1)
+
+        if (rand.nextInt(9) == 7) {
+            GameRunner.notAWinner = aGame.wrongAnswer()
+        } else {
+            GameRunner.notAWinner = aGame.wasCorrectlyAnswered()
+        }
+
+
+    } while (GameRunner.notAWinner)
+
+}
---
+++ src/test/kotlin/com/refacgtoring/trivia/SomeTest.kt
@@ -0,0 +1,12 @@
+package com.refacgtoring.trivia
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class SomeTest {
+
+    @Test
+    fun someTest() {
+        assertTrue(false)
+    }
+}
